abd3a9a3e954c5f74190b0a90b2c6c074fb5a223
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-10-25 18:46:19 -0700

    Removed dates on sale order line.

--- models/sale_order.py
+++ models/sale_order.py
@@ -31,23 +31,3 @@ class SaleOrder(models.Model):
             mfg_orders = order.env['mrp.production'].search([('procurement_group_id.name','=',order.name)])
             for mfg in mfg_orders:
                 mfg.button_plan()
-
-
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-
-    requested_date = fields.Datetime(string='Requested Date',
-    index=True, readonly=False, store=True, copy=False, compute='_get_dates')
-    commitment_date = fields.Datetime(string='Commitment Date',
-    index=True, readonly=False, store=True, copy=False, compute='_get_dates')
-
-
-
-    @api.onchange('product_id')
-    def _get_dates(self):
-        for line in self:
-            #special for client because of former custom date
-            if line.requested_date == False:
-                line.requested_date = line.order_id.x_requested_date
-            if line.commitment_date == False:
-                line.commitment_date = line.order_id.x_commitment_date

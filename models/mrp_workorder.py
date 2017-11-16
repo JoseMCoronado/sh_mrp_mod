6fa152c0663867f73e75672ed416b811fbd6df20
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-11-15 21:24:23 -0800

    Disable the 'Not Enough Inventory' warning on the sale.order.line Changed sale_workorder form view Changed mrp_workorder tree view Set operator default to 1 in mrp_workcenter_entry_wizard (if they would like to use in the future) Simplified the workcenter-workorder completion process. Now they only have to click on Done. Once the user clicks on Done on the workcenter-workorder completion process, the 'Completed By' field is populated with that user's id.

--- models/mrp_workorder.py
+++ models/mrp_workorder.py
@@ -6,9 +6,19 @@ class MrpWorkorder(models.Model):
     _inherit = 'mrp.workorder'
 
     sale_workorder_id = fields.Many2one('sale.workorder', 'Sale Workorder', readonly="1", store="1", related='production_id.sale_workorder_id')
+    requested_date = fields.Datetime(string='Requested Date',
+    readonly=True, store=True, index=True, copy=False,related='production_id.sale_workorder_id.requested_date')
+    commitment_date = fields.Datetime(string='Commitment Date',
+    readonly=True, store=True, index=True, copy=False,related='production_id.sale_workorder_id.commitment_date')
+    done_user = fields.Many2one('res.users', string='Completed By')
 
     @api.multi
     def button_finish(self):
         super(MrpWorkorder, self).button_finish()
         if all(wo.state == 'done' for wo in self.production_id.workorder_ids):
             self.production_id.button_mark_done()
+
+    @api.multi
+    def record_production(self):
+        super(MrpWorkorder, self).record_production()
+        self.done_user = self.env.user
--- models/sale_order.py
+++ models/sale_order.py
@@ -73,3 +73,19 @@ class SaleOrder(models.Model):
                     'user_id': order.user_id.id,
                 }
                 sale_workorder = order.env['sale.workorder'].create(sale_workorder_values)
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.onchange('product_uom_qty', 'product_uom', 'route_id')
+    def _onchange_product_id_check_availability(self):
+        if not self.product_id or not self.product_uom_qty or not self.product_uom:
+            self.product_packaging = False
+            return {}
+        if self.product_id.type == 'product':
+            precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+            product_qty = self.product_uom._compute_quantity(self.product_uom_qty, self.product_id.uom_id)
+            if float_compare(self.product_id.virtual_available, product_qty, precision_digits=precision) == -1:
+                is_available = self._check_routing()
+
+        return {}

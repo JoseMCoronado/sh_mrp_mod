8696c1f3fc22fbe94ef88e86736fed8f7d8133ac
Jose M. Coronado <j.coronado.personal@gmail.com>
2018-08-28 18:26:10 -0700

    [FIX] Create --> Inherit

--- models/shipping_account.py
+++ models/shipping_account.py
@@ -10,13 +10,6 @@ class ShippingAccount(models.Model):
     sequence = fields.Integer('Sequence', default=1, help="Lower is better.")
     x_acct_num = fields.Char(string="Account Number")
     x_carrier_id = fields.Many2one('delivery.carrier',string="Carrier")
-    x_name = fields.Char(string="Name",compute="_compute_name",readonly=True)
     x_notes = fields.Char(string="Notes")
     x_partner_id = fields.Many2one('res.partner',string="Customer")
     x_zip = fields.Char(string="Billing Zip")
-
-    @api.depends('x_partner_id','x_acct_num','x_carrier_id')
-    def _compute_name(self):
-        for record in self:
-            if record.x_partner_id and record.x_acct_num and record.x_carrier_id:
-                record.x_name = "%s %s %s" % (record.x_partner_id,ecord.x_acct_num,record.x_carrier_id)
